623616c4b5d411ea7d971139a06ce094601115d6
eshwarsundar <northeasteditsofficial@gmail.com>
2022-01-02 22:54:59 +0530

    Add Splashscreen animation
    To add a splashscreen animation,
    1. I have created a Navigation with a navigation Link
    2. Navigation link takes a destination view and isActive boolean binding
    3. Embed an empty view as it is required
    4. When ZStack appears, trigger a state change on the UI after 3 seconds
    5. Since the state change occurs on the UI, we need to use a DispatchQueue.main.asyncAfter timeout function

---
+++ StudyCards/Views/HomePageView.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftUIView.swift
+//  StudyCards
+//
+//  Created by Eshwar S on 02/01/22.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    var body: some View {
+        VStack{
+            
+        }
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePageView()
+    }
+}
