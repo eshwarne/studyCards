6c766193baf7486d17ec1ceec35b0cef9b33e51a
eshwarsundar <northeasteditsofficial@gmail.com>
2022-01-02 14:51:34 +0530

    Initial Commit

---
+++ StudyCards/StudyCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyCardsApp.swift
+//  StudyCards
+//
+//  Created by Eshwar S on 02/01/22.
+//
+
+import SwiftUI
+
+@main
+struct StudyCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
