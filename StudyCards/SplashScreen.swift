2c346e6c606833d4221dbecea79d65642b6249ef
eshwarsundar <northeasteditsofficial@gmail.com>
2022-01-02 23:48:48 +0530

    Complete home view
    1. Add two buttons with no action and with text inside
    2. Use NavigationBarBackButtonHidden set to true to make sure back button does not appear on the UI
    3. Use clipshape over the view to clip the view using the shape as a mask. If clipshape is used before the background it does not work, as the view is not actually clipped it is just masked

--- StudyCards/SplashScreen.swift
+++ StudyCards/SplashScreen.swift
@@ -9,33 +9,44 @@ import SwiftUI
 
 
 struct SplashScreen: View {
+    @State var isHomePageActive: Bool = false
     var body: some View {
-        ZStack{
-            Color(0xF4F4F4)
-                .edgesIgnoringSafeArea(.all)
-            VStack(spacing:40){
-                Text("Study Cards")
-                    .font(.system(size: 45))
-                    .fontWeight(.bold)
-                ZStack{
-                    RoundedRectangle(cornerRadius: 10)
-                        .frame(width:101,height:147)
-                        .foregroundColor(Color(0x3A3535))
-                    RoundedRectangle(cornerRadius: 10)
-                        .frame(width:101,height:147)
-                        .foregroundColor(Color(0xFF7315))
-                        .offset(CGSize(width: -10, height: 10))
-                    RoundedRectangle(cornerRadius: 10)
-                        .frame(width:101,height:147)
-                        .foregroundColor(Color(0x232020))
-                        .offset(CGSize(width: -20, height: 20))
-                }.offset(CGSize(width: 0, height: -20))
+        NavigationView {
+            ZStack{
+                NavigationLink(destination:HomePageView(), isActive: $isHomePageActive){
+                        EmptyView()
+                }
+                Color(0xF4F4F4)
+                    .edgesIgnoringSafeArea(.all)
+                VStack(spacing:50){
+                    Text("Study Cards")
+                        .font(.system(size: 45))
+                        .fontWeight(.bold)
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(width:101,height:147)
+                            .foregroundColor(Color(0x3A3535))
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(width:101,height:147)
+                            .foregroundColor(Color(0xFF7315))
+                            .offset(CGSize(width: -10, height: 10))
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(width:101,height:147)
+                            .foregroundColor(Color(0x232020))
+                            .offset(CGSize(width: -20, height: 20))
+                    }.offset(CGSize(width: 0, height: -20))
+                    
+                    Text("Create, Share and Study cards")
+                        .font(.system(size: 12))
+                        .tracking(3)
+                        .bold()
                 
-                Text("Create, Share and Study cards")
-                    .font(.system(size: 12))
-                    .tracking(3)
-                    .bold()
-            
+                }
+            }
+        }
+        .onAppear{
+            DispatchQueue.main.asyncAfter(deadline: .now()+3){
+                self.isHomePageActive = true
             }
         }
         
--- StudyCards/Views/HomePageView.swift
+++ StudyCards/Views/HomePageView.swift
@@ -9,9 +9,42 @@ import SwiftUI
 
 struct HomePageView: View {
     var body: some View {
-        VStack{
-            
-        }
+        ZStack{
+            Color(0xF4F4F4)
+                .edgesIgnoringSafeArea(.all)
+            VStack(alignment:.center, spacing: 40){
+                Text("What are you upto today?")
+                HStack{
+                    Button(action:{}){
+                        Text("VIEW CARDS")
+                            .fontWeight(.bold)
+                            .padding()
+                            
+                            .foregroundColor(Color(0xF4F4F4))
+                            .frame(width: 100, height: 100, alignment: .center)
+                            .background(Color(0xFF7315))
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            
+                    }
+                    
+                    Button(action:{}){
+                        Text("CREATE CARDS")
+                            .fontWeight(.bold)
+                            .padding()
+                            .foregroundColor(Color(0xF4F4F4))
+                            .frame(width: 100, height: 100, alignment: .center)
+                            .background(Color(0x3A3535))
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                        
+                            
+                    }
+                    
+                }
+              
+               
+            }
+        }.navigationBarBackButtonHidden(true)
+       
     }
 }
 
